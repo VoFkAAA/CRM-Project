841589d7329f47e1a3763cf80b8fa2dcbea78c88
unknown <arhiv.1988@mail.ru>
2026-06-08 10:34:31 +0300

    Добавлен файл amvera.yaml, изменен файл config_prod - добавлено SSL соединение

--- config_prod.py
+++ config_prod.py
@@ -17,6 +17,10 @@ class Config:
     if not DATABASE_URL:
         raise ValueError("DATABASE_URL не задан на сервере")
 
-    SQLALCHEMY_DATABASE_URI = DATABASE_URL
+    # Добавляем SSL для подключения Supabase к Amvera (если в URL нет sslmode)
+    if "supabase" in DATABASE_URL and "sslmode" not in DATABASE_URL:
+        separator = "?" if "?" not in DATABASE_URL else "&"
+        DATABASE_URL += f"{separator}sslmode=require"
 
+    SQLALCHEMY_DATABASE_URI = DATABASE_URL
     SQLALCHEMY_ENGINE_OPTIONS = {"connect_args": {"options": "-c client_encoding=utf8"}}
